610169ba6784c157fb8ab17f4f5276a929cf3399
Ryan Oloughlin <rmoloughlin11@gmail.com>
2021-02-17 10:43:56 +0100

    The beginnings of data analysis for wind proposition

---
+++ data_analysis/wind_plot.py
@@ -0,0 +1,31 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
+
+
+def get_data():
+
+    wind_dat = pd.read_csv("Turbine_Data.csv", sep=',')
+
+    wind_dat.rename(columns = {"Unnamed: 0": "Time"}, inplace=True)
+
+    # print(wind_dat.shape)
+    # print(wind_dat.columns)
+
+    return wind_dat
+
+def plot_power(wind_dat):
+
+    plt.plot(wind_dat['Time'][146:1000], wind_dat['ActivePower'][146:1000])
+    plt.title("Wind Power Output over Time")
+    plt.xlabel("Time (10-minute Intervals)")
+    plt.ylabel("Active Power")
+    plt.show()
+
+
+def main():
+    wind_dat = get_data()
+    plot_power(wind_dat)
+
+main()
